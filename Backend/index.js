f1acd0a3472c333031a7ac561e48d6d738594f11
ANDERSON DA SILVA FERNANDES <64322982+Anderson2022@users.noreply.github.com>
2022-09-10 09:34:57 -0400

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is ahead of 'origin/main' by 2 commits.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	modified:   Backend/index.js
    	deleted:    Backend/routes.js

--- Backend/index.js
+++ Backend/index.js
@@ -4,6 +4,26 @@ const server = express();
 const db = []
 server.use(express.json())
 
+server.get('/', (req,res) => {
+    return res.json(db)
+});
+
+server.post('/', (req,res) => {
+    const { body } = req
+    db.push(body)
+});
+
+server.put('/', (req,res) => {
+    return res.json(db)
+});
+
+server.delete('/:id', (req,res) => {
+    return res.json(db)
+});
+
+server.listen(3000, () => {
+    console.log('Servidor esta funcionando');
+});
 
 
 
